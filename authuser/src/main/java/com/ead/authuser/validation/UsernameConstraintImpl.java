bad7af39b380db44714e5c3ff8a4dd110ec9326c
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-28 13:37:59 -0300

    validação customizada

---
+++ authuser/src/main/java/com/ead/authuser/validation/UsernameConstraintImpl.java
@@ -0,0 +1,19 @@
+package com.ead.authuser.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UsernameConstraintImpl implements ConstraintValidator<UsernameConstraint, String> {
+    @Override
+    public void initialize(UsernameConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
+        if (username == null || username.trim().isEmpty() || username.contains(" ")){
+            return false;
+        }
+        return true;
+    }
+}
