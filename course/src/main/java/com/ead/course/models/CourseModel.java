f36924b2528fa648b5d51d59ef2b7085bf7497f7
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-11-04 03:35:55 -0300

    Corrigindo bug ao excluir curso com filhos

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -47,7 +47,7 @@ public class CourseModel implements Serializable {
     private UUID userInstructor;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
     @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
 }
