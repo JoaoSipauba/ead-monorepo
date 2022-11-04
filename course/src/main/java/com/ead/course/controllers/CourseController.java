467e79f98830cecf04613d04b7bb73235a7b74d2
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-11-04 18:41:51 -0300

    filtros avançados com specification e pageable

--- course/src/main/java/com/ead/course/controllers/CourseController.java
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -3,8 +3,13 @@ package com.ead.course.controllers;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
+import com.ead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -63,8 +68,9 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CourseModel>> getAllCourses(){
-        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll());
+    public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
+                                                           @PageableDefault(size = 10, page = 0, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
     }
 
     @GetMapping("/{courseId}")
---
+++ course/src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -0,0 +1,34 @@
+package com.ead.course.specifications;
+
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "courseStatus", spec = Equal.class),
+            @Spec(path = "nome", spec = Like.class)
+    })
+    public interface CourseSpec extends Specification<CourseModel> {}
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "courseStatus", spec = Equal.class),
+            @Spec(path = "nome", spec = Like.class)
+    })
+    public interface ModuleSpec extends Specification<ModuleModel> {}
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "courseStatus", spec = Equal.class),
+            @Spec(path = "nome", spec = Like.class)
+    })
+    public interface LessonSpec extends Specification<LessonModel> {}
+}
